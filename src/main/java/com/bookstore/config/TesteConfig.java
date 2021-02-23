d4cd68fdcc66f729a3d3394358aba5f9c354efed
robsonKirka <robsonfernandes_job@hotmail.com>
2021-02-22 22:13:49 -0300

    Adição do profile test spring

---
+++ src/main/java/com/bookstore/config/TesteConfig.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.bookstore.service.DBService;
+
+/**
+ * @author robson.kirka
+ *
+ */
+@Configuration
+@Profile("test")
+public class TesteConfig {
+	
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void createDataBase() {
+		this.dbService.createDataBase();
+		
+	}
+}
